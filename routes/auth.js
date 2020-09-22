fc4f14bd3b8c0c42f6489f2e3365b058b13a5819
Jonatelo09 <kuroko.arevalo@gmail.com>
2020-09-22 13:41:39 -0500

    Backend Chat Flutter

---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+/*
+    path: api/login
+*/
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { crearUsuario, loginUsuario, renewToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+router.post('/new', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio y debe ser valido').isEmail(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    validarCampos
+], crearUsuario);
+
+router.post('/', [
+    check('email', 'El email es obligatorio y debe ser valido').isEmail(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    validarCampos
+
+], loginUsuario);
+
+//validarJWT
+
+router.get('/renew', validarJWT, renewToken);
+
+module.exports = router;
